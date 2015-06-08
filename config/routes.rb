d1757e4e29960d83667cbf4cf9d9bd7738971a6d
Elia Caterina <eliacaterina24@gmail.com>
2015-06-08 09:37:49 +0800

    more css changes, and I installed the infinite scroll bower package

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#index'
 
-  resources :posts, :only => [:create, :show, :index, :destroy, :update, :current] 
+  resources :posts, :only => [:create, :show, :index, :destroy, :update, :current]  
 
   get '/current_user', to: 'users#current' #users is the controller, current is the method
   get '/users/:id/posts', to: 'posts#userfeed'
